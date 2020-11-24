8b6b35cd5e1407b75414c784de91d29816a50341
repl.it user <replituser@example.com>
2020-11-24 17:16:06 +0000

    Aggiunto esempio lezione 09.10.2020

---
+++ Lezione_for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//09.10.2020
+//Lezione introduttiva sulla struttura iterativa for
+
+int main() 
+{
+int n = 5;
+int f = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    f = f * i;
+}
+
+printf("Il fattoriale di 5 è: %d", f);
+
+return 0;
+}
