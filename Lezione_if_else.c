f79a89797ddd37f90af7c9ded248d8e26d37d129
repl.it user <replituser@example.com>
2020-11-24 17:02:09 +0000

    Aggiunto esempio lezione 09.10.2020

---
+++ Lezione_if_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//09.10.2020
+//Lezione introduttiva sulle strutture condizionali if/else
+
+//Calcolo del MAX con if/else
+int main() 
+{
+int x = 10;
+int y = 5;
+int max;
+
+if (x > y){
+    max = x;
+}
+else
+    max = y;
+
+printf("Il MAX è: %d", max);
+
+return 0;
+}
